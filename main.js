c9e309b7b07f73750c9e98101a023a61b54bf367
Rizal Maulana <rizalfmaulana@gmail.com>
2021-03-10 13:19:01 +0700

    add responsive

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var email = document.getElementById("email");
 var text = document.querySelector(".valid");
 var form = document.querySelector(".box");
+var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
